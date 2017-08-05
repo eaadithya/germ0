88980bf52c1916896563d668dd2dbe59fb19cb50
RadikaDilanka <radikadilanka@gmail.com>
2017-08-05 15:48:16 +0530

    germZero

---
+++ OwnerFunctions/addItem.php
@@ -0,0 +1,40 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+           <meta charset="utf-8">
+           <meta http-equiv="X-UA-Compatible" content="IE=edge">
+           <meta name="viewport" content="width=device-width, initial-scale=1">
+         <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        
+        <title>
+            add Item 
+        </title>
+        
+    </head>
+    
+    
+    <body>
+        
+        <div class="panel panel-default">
+             <div class="panel-heading">Panel Heading</div>
+             <div class="panel-body">Panel Content</div>
+            
+            
+        </div>
+        
+        
+    </body>
+    
+</html>
